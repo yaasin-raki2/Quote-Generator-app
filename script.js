bf3ac77fc553ba055f7e636b3bf207331733b96f
Yassine <yassin23raki@gmail.com>
2020-09-08 01:02:10 +0100

    A loading animation has been added

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if(!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote from API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://ancient-thicket-78623.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -24,6 +40,8 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // Stop loader, Show Quote
+        complete();
     } catch (error) {
         getQuote();
     }
@@ -43,3 +61,4 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // on load
 getQuote();
+
